3aca1e3e92edefb1deffbe4dc1caca9a476d4e22
Matthew Pearson <matthewpearson@gmail.com>
2014-09-11 14:55:40 -0700

    Improve ctags.py's flag handling and log messages when path is '.'.

--- scripts/ctags.py
+++ scripts/ctags.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2014 Logos Research Systems.
-#
 # Code derives from demo code in the BBEdit User Manual 10.5.5, available
 # at http://pine.barebones.com/manual/BBEdit_10_User_Manual.pdf (p. 308),
-# which is Copyright (c) 1992-2013 Bare Bones Software, Inc.
+# contents of which are Copyright (c) 1992-2013 Bare Bones Software, Inc.
 #
 # $Id$
 # (re)builds a BBEdit-compatible set of ctags within the specified directory
@@ -17,24 +15,27 @@ import sys
 import tempfile
 import time
 
+FIELD_FLAGS = "--fields=+a+k+m+n+S"
+KIND_FLAGS = " ".join(["--C#-kinds=+cEigmnst-efl", "--Java-kinds=+cei-flmp", "--Python-kinds=+cfm"])
+
 if len(sys.argv) != 2:
-	print >> sys.stderr, "usage: ctags.py <path_to_repository>"
+	script_name = os.path.basename(sys.argv[0]) if len(sys.argv) != 0 else "ctags.py"
+	print >> sys.stderr, "usage: %s <path_to_repository>" % script_name
 	sys.exit(1)
 
 proj_path = sys.argv[1]
-# proj_path = os.environ['SRCROOT']
 
 if not os.access(proj_path, os.F_OK):
 	print >> sys.stderr, "error: can't access requested directory %s" % proj_path
 
 ctags_bin = "/Applications/BBEdit.app/Contents/Helpers/ctags"
-ctags_arg = "--excmd=number --tag-relative=no --fields=+a+m+n+S -R"
+ctags_arg = "--extra=+f-q --excmd=number --file-scope=yes --tag-relative=no %s %s -R" % (FIELD_FLAGS, KIND_FLAGS)
 ctags_tmp = tempfile.NamedTemporaryFile(prefix="ctags")
 ctags_cmd = "'%s' %s -f '%s' '%s'" % (ctags_bin, ctags_arg, ctags_tmp.name, proj_path)
 ctags_err = tempfile.NamedTemporaryFile(prefix="ctags.stderr")
 
 try:
-	print >> sys.stderr, "tagging %s ..." % proj_path,
+	print >> sys.stderr, "tagging %s ..." % os.path.abspath(proj_path),
 	start_time = time.time()
 	ret = subprocess.call(ctags_cmd, shell=True, stderr=ctags_err)
 	end_time = time.time()
